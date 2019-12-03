104796e42e358900383da92e17ed2d059a473892
Katherine <katherine.szelag@mail.utoronto.ca>
2019-12-03 19:08:27 +0000

    initial commit

---
+++ timer.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+
+for (let time of args) {
+  if (time >= 0 && typeof Number(time) === 'number')
+  setTimeout(() => {
+    process.stdout.write('\x07');
+    
+  }, Number(time) * 1000)
+}
